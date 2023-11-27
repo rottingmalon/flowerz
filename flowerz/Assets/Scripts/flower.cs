13916184cad3feb3afa6b31ab3cf1500d88ae2f1
MeezHu <75973028+rottingmalon@users.noreply.github.com>
2023-11-27 10:39:08 +0100

    creation

---
+++ flowerz/Assets/Scripts/flower.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class flower 
+{
+    #region VAR
+    private float growAmount;
+    private float fuseRadius;
+    private float produceAmount;
+    private float produceTime;
+    private string[] fusions;
+    private string attribute;
+
+    #endregion
+
+    #region FUN
+    private void produce() 
+    {
+    }
+
+    private void grow() 
+    {
+    }
+    #endregion
+}
