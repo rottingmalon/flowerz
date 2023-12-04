431767d6400b97072f7efc1e6f2e2cf607bf37e0
MeezHu <75973028+rottingmalon@users.noreply.github.com>
2023-12-04 17:55:27 +0100

    different flowers + setup suite

--- flowerz/Assets/Scripts/ControlsManager.cs
+++ flowerz/Assets/Scripts/ControlsManager.cs
@@ -4,19 +4,61 @@ using UnityEngine;
 
 public class ControlsManager : MonoBehaviour
 {
-    [SerializeField] private GameObject flower;
+    [SerializeField] private GameObject redFlower;
+    [SerializeField] private GameObject blueFlower;
+    [SerializeField] private GameObject greenFlower;
+
+    private int SelectedFlower = 1;
 
     void Update()
     {
+        SelectedFlower = CheckFlower();
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(r, out hit)) 
+            if (Physics.Raycast(r, out hit) && hit.transform.tag == "Ground") 
             {
-                Instantiate(flower, hit.point, Quaternion.identity);
+                switch (SelectedFlower) 
+                {
+                    case 1:
+                        Instantiate(redFlower, hit.point, Quaternion.identity);
+                        break;
+                    case 2:
+                        Instantiate(blueFlower, hit.point, Quaternion.identity);
+                        break;
+                    case 3:
+                        Instantiate(greenFlower, hit.point, Quaternion.identity);
+                        break;
+                    default: 
+                        break;
+                }
             }
         }
+    }
+
+    private int CheckFlower() 
+    {
+        int i = SelectedFlower;
+
+        if (Input.GetKeyUp("1") && i != 1)
+        {
+            i = 1;
+            Debug.Log("1");
+        }
+        else if (Input.GetKeyUp("2") && i != 2)
+        {
+            i = 2;
+            Debug.Log("2");
+        }
+        else if (Input.GetKeyUp("3") && i != 3)
+        {
+            i = 3;
+            Debug.Log("3");
+        }
+
+        return (i);
     }
 }
--- flowerz/Assets/Scripts/flower.cs
+++ flowerz/Assets/Scripts/flower.cs
@@ -8,19 +8,23 @@ public class flower
     #region VAR
     private float growAmount;
     private float fuseRadius;
-    private float produceAmount;
-    private float produceTime;
+    //private float produceAmount;
+    //private float produceTime;
     private string[] fusions;
     private string attribute;
 
     #endregion
 
     #region FUN
-    private void produce() 
+    /*private void produce() 
+    {
+    }*/
+
+    private void grow() 
     {
     }
 
-    private void grow() 
+    private void fuse() 
     {
     }
     #endregion
