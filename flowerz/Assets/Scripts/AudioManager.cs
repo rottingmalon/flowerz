0d4e140e132442bb0895078858d87b31411dbdd1
rottingmalon <75973028+rottingmalon@users.noreply.github.com>
2024-01-24 19:12:03 +0100

    options menu

--- flowerz/Assets/Scripts/AudioManager.cs
+++ flowerz/Assets/Scripts/AudioManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
@@ -9,6 +10,11 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private GameObject dirtSfx;
     [SerializeField] private GameObject fusionObject;
 
+    [Space]
+    [SerializeField] private AudioMixer MasterMixer;
+    [SerializeField] private AudioMixer MusicMixer;
+    [SerializeField] private AudioMixer SFXMixer;
+    
     private AudioSource _dirtSource;
     private AudioSource _fusionSource;
 
